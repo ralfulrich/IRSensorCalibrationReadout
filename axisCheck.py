0ba1112dde474ca3fd4626a1ee19b4972bb7485e
sebaur <sebastian.baur@kit.edu>
2016-07-06 12:44:47 +0200

    bugfixes, now working

--- axisCheck.py
+++ axisCheck.py
@@ -21,24 +21,25 @@ def doReferenceTravel(axis):
     send(usb, "REF"+str(axis)+"=6") # drive to maximum -> minimum:: set minimum to zero
     while send(usb, "?ASTAT") == ('?ASTAT', 'P'):# check, if stage is moving or reached target positon
         time.sleep(1)
-    print '>reference travel done'
+    print 'Reference travel done'
 
 def setTargetAndGo(axis,target):
-    send(usb, "PVEL"+str(axis)+"=" + str(maxVelocity)) # set max velocity
+    send(usb, "PVEL"+str(axis)+"=" + str(velocity)) # set max velocity
     send(usb, "ACC"+str(axis)+"=" + str(accerleration)) # set accerleration
     send(usb, "PSET"+str(axis)+"="+str(target)) # set target position
     send(usb, "PGO"+str(axis)+"")# start positioning the stage
     while send(usb, "?ASTAT")[1] == 'T': # wait till movement is finished
         time.sleep(1)
+    print 'Reached position at ' + str(int(send(usb, "?CNT1")[1])/10000.) +' mm'
 
 def getRailStatus(axis):
-    print send(usb, "?ASTAT") # stage status ausgaben
-    print send(usb, "?CNT"+str(axis)+"") # current position
+    print 'Status of axis ' + str(axis) + ': ' + send(usb, "?ASTAT")[1] # stage status ausgaben
+    print 'Cuttent position: ' + send(usb, "?CNT"+str(axis)+"")[1] # current position
 
-def turnOff():
-    send(usb, "MOFF")
-    print '>axis turnes off'
-    print '>bye!'
+def turnOff(axis):
+    print 'Turning of axis ' + str(axis)
+    send(usb, "MOFF"+str(axis))
+
 
 ######################################################
 ######################################################
@@ -47,25 +48,26 @@ usb = serial.Serial("/dev/ttyACM0")
 usb.baudrate = 9600
 usb.timeout = 0.2
 
-if not len(sys.argv) == 1:
+if not len(sys.argv) == 2:
     print 'You need to give the axis number'
     print 'Exiting...'
-    return
+    exit
 
-axis = int(sys.argv[0])
+axis = int(sys.argv[1])
 
 send(usb, "TERM=2") # response with plain text and 'OK'
 
 send(usb, "INIT"+str(axis)) # initialize the stage
-print '>initialization of axis ' +str(axis) +' 1 done'
+print 'Initialization of axis ' +str(axis) +' done'
 
 doReferenceTravel(axis)
 
-setTargetAndGo(axis,200000)
-print '>reached position at ' + str(int(send(usb, "?CNT1")[1])/10000.) +' mm'
+setTargetAndGo(axis,300000)
 
 getRailStatus(axis)
 
-turnOff()
+turnOff(axis)
 
-return
+getRailStatus(axis)
+
+exit
